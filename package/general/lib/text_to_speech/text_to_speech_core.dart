7d67ebedb495d44fdb68fc623bcb5b5d32b5ee68
Your Name <you@example.com>
2024-03-15 08:14:36 +0700

    update

--- package/general/lib/text_to_speech/text_to_speech_core.dart
+++ package/general/lib/text_to_speech/text_to_speech_core.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'package:general/player/player_controller.dart';
+
 class GeneralLibraryTextToSpeechBase {
+  static bool is_text_to_speech_on_speak = false;
   GeneralLibraryTextToSpeechBase();
 
   Future<void> dispose() async {}
@@ -8,10 +11,155 @@ class GeneralLibraryTextToSpeechBase {
 
   Future<void> speak({
     required String text,
+    bool isWaithFinishedSpeakBefore = false,
+    Duration? durationWaitFinishedSpeakBefore,
     double volume = 1.0,
     double pitch = 1.0,
     double rate = 0.5,
-  }) async {}
+  }) async {
+    if (isWaithFinishedSpeakBefore) {
+      durationWaitFinishedSpeakBefore ??= Duration(minutes: 1);
+      DateTime dateTime_expire = DateTime.now().add(durationWaitFinishedSpeakBefore);
+      while (true) {
+        await Future.delayed(Duration(milliseconds: 1));
+        if (dateTime_expire.isBefore(DateTime.now())) {
+          return;
+        }
+        break;
+      }
+    }
+  }
 
+  Future<void> speakWithAutoSetVolume({
+    required String text,
+    required GeneralLibraryPlayerControllerBase player,
+    double volume_default = 100,
+    required double volume_low,
+    double volume_space = 5,
+    bool isWaithFinishedSpeakBefore = false,
+    Duration? durationWaitFinishedSpeakBefore,
+    double volume = 1.0,
+    double pitch = 1.0,
+    double rate = 0.5,
+  }) async {
+    await setVolumeDown(
+      player: player,
+      from: volume_default,
+      to: volume_low,
+      decrease: volume_space,
+    );
+    await speak(
+      text: text,
+      isWaithFinishedSpeakBefore: isWaithFinishedSpeakBefore,
+      durationWaitFinishedSpeakBefore: durationWaitFinishedSpeakBefore,
+      volume: volume,
+      pitch: pitch,
+      rate: rate,
+    );
+    await setVolumeUp(
+      player: player,
+      to: volume_default,
+      from: volume_low,
+      increase: volume_space,
+    );
+  }
 
+  Future<void> setVolumeDown({
+    required GeneralLibraryPlayerControllerBase player,
+    double from = 100,
+    required double to,
+    double decrease = 5,
+  }) async {
+    if (from > 100) {
+      from = 100;
+    }
+    for (double i = from; i >= to; (i -= decrease)) {
+      await Future.delayed(const Duration(milliseconds: 10));
+      // await player.setVolume(i);
+    }
+  }
+
+  Future<void> setVolumeUp({
+    required GeneralLibraryPlayerControllerBase player,
+    required double from,
+    double to = 100,
+    double increase = 5,
+  }) async {
+    if (to > 100) {
+      to = 100;
+    }
+    for (double i = from; i <= to; (i += increase)) {
+      await Future.delayed(const Duration(milliseconds: 10));
+
+      // print(i);
+      // await player.setVolume(i);
+    }
+  }
+
+  Future<void> speakWithAutoSetVolumes({
+    required String text,
+    required List<GeneralLibraryPlayerControllerBase> players,
+    double volume_default = 100,
+    required double volume_low,
+    double volume_space = 5,
+    bool isWaithFinishedSpeakBefore = false,
+    Duration? durationWaitFinishedSpeakBefore,
+    double volume = 1.0,
+    double pitch = 1.0,
+    double rate = 0.5,
+  }) async {
+    await setVolumeDowns(
+      players: players,
+      from: volume_default,
+      to: volume_low,
+      decrease: volume_space,
+    );
+    await speak(
+      text: text,
+      isWaithFinishedSpeakBefore: isWaithFinishedSpeakBefore,
+      durationWaitFinishedSpeakBefore: durationWaitFinishedSpeakBefore,
+      volume: volume,
+      pitch: pitch,
+      rate: rate,
+    );
+    await setVolumeUps(
+      players: players,
+      to: volume_default,
+      from: volume_low,
+      increase: volume_space,
+    );
+  }
+
+  Future<void> setVolumeDowns({
+    required List<GeneralLibraryPlayerControllerBase> players,
+    double from = 100,
+    required double to,
+    double decrease = 5,
+  }) async {
+    for (var i = 0; i < players.length; i++) {
+      await setVolumeDown(
+        player: players[i],
+        to: to,
+        from: from,
+        decrease: decrease,
+      );
+    }
+  }
+
+  Future<void> setVolumeUps({
+    required List<GeneralLibraryPlayerControllerBase> players,
+    required double from,
+    double to = 100,
+    double increase = 5,
+  }) async {
+    for (var i = 0; i < players.length; i++) {
+      await setVolumeUp(
+        player: players[i],
+        to: to,
+        from: from,
+        increase: increase,
+      );
+    }
+  
+  }
 }
--- package/general_flutter/lib/text_to_speech/text_to_speech_core.dart
+++ package/general_flutter/lib/text_to_speech/text_to_speech_core.dart
@@ -1,12 +1,14 @@
 // ignore_for_file: non_constant_identifier_names, empty_catches
 
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:general/player/player_controller.dart';
 import 'package:general/text_to_speech/text_to_speech_core.dart';
 import 'package:general_lib/general_lib.dart';
 
 class GeneralLibraryTextToSpeechBaseFlutter implements GeneralLibraryTextToSpeechBase {
   FlutterTts flutterTts = FlutterTts();
 
+  static bool is_text_to_speech_on_speak = false;
   bool get isIOS => !dart.isWeb && dart.isIOS;
   bool get isAndroid => !dart.isWeb && dart.isAndroid;
   bool get isWindows => !dart.isWeb && dart.isWindows;
@@ -41,17 +43,32 @@ class GeneralLibraryTextToSpeechBaseFlutter implements GeneralLibraryTextToSpeec
   @override
   Future<void> speak({
     required String text,
+    bool isWaithFinishedSpeakBefore = false,
+    Duration? durationWaitFinishedSpeakBefore,
     double volume = 1.0,
     double pitch = 1.0,
     double rate = 0.5,
   }) async {
     if (text.trim().isNotEmpty) {
       if (flutter_is_support_flutter_tts) {
+        if (isWaithFinishedSpeakBefore) {
+          durationWaitFinishedSpeakBefore ??= const Duration(minutes: 1);
+          DateTime dateTime_expire = DateTime.now().add(durationWaitFinishedSpeakBefore);
+          while (true) {
+            await Future.delayed(const Duration(milliseconds: 1));
+            if (dateTime_expire.isBefore(DateTime.now())) {
+              return;
+            }
+            break;
+          }
+        }
+        isWaithFinishedSpeakBefore = true;
 
         await flutterTts.setVolume(1.0);
         await flutterTts.setSpeechRate(rate);
         await flutterTts.setPitch(pitch);
         await flutterTts.speak(text.trim());
+        isWaithFinishedSpeakBefore = false;
       }
     }
   }
@@ -64,5 +81,143 @@ class GeneralLibraryTextToSpeechBaseFlutter implements GeneralLibraryTextToSpeec
         return;
       }
     } catch (e) {}
+  }
+
+  @override
+  Future<void> speakWithAutoSetVolume({
+    required String text,
+    required GeneralLibraryPlayerControllerBase player,
+    double volume_default = 100,
+    required double volume_low,
+    double volume_space = 5,
+    bool isWaithFinishedSpeakBefore = false,
+    Duration? durationWaitFinishedSpeakBefore,
+    double volume = 1.0,
+    double pitch = 1.0,
+    double rate = 0.5,
+  }) async {
+    await setVolumeDown(
+      player: player,
+      from: volume_default,
+      to: volume_low,
+      decrease: volume_space,
+    );
+    await speak(
+      text: text,
+      isWaithFinishedSpeakBefore: isWaithFinishedSpeakBefore,
+      durationWaitFinishedSpeakBefore: durationWaitFinishedSpeakBefore,
+      volume: volume,
+      pitch: pitch,
+      rate: rate,
+    );
+    await setVolumeUp(
+      player: player,
+      to: volume_default,
+      from: volume_low,
+      increase: volume_space,
+    );
+  }
+
+  @override
+  Future<void> setVolumeDown({
+    required GeneralLibraryPlayerControllerBase player,
+    double from = 100,
+    required double to,
+    double decrease = 5,
+  }) async {
+    if (from > 100) {
+      from = 100;
+    }
+    for (double i = from; i >= to; (i -= decrease)) {
+      await Future.delayed(const Duration(milliseconds: 10));
+      await player.setVolume(i);
+    }
+  }
+
+  @override
+  Future<void> setVolumeUp({
+    required GeneralLibraryPlayerControllerBase player,
+    required double from,
+    double to = 100,
+    double increase = 5,
+  }) async {
+    if (to > 100) {
+      to = 100;
+    }
+    for (double i = from; i <= to; (i += increase)) {
+      await Future.delayed(const Duration(milliseconds: 10));
+ 
+      await player.setVolume(i);
+    }
+  }
+
+  @override
+  Future<void> speakWithAutoSetVolumes({
+    required String text,
+    required List<GeneralLibraryPlayerControllerBase> players,
+    double volume_default = 100,
+    required double volume_low,
+    double volume_space = 5,
+    bool isWaithFinishedSpeakBefore = false,
+    Duration? durationWaitFinishedSpeakBefore,
+    double volume = 1.0,
+    double pitch = 1.0,
+    double rate = 0.5,
+  }) async {
+    await setVolumeDowns(
+      players: players,
+      from: volume_default,
+      to: volume_low,
+      decrease: volume_space,
+    );
+    await speak(
+      text: text,
+      isWaithFinishedSpeakBefore: isWaithFinishedSpeakBefore,
+      durationWaitFinishedSpeakBefore: durationWaitFinishedSpeakBefore,
+      volume: volume,
+      pitch: pitch,
+      rate: rate,
+    );
+    await setVolumeUps(
+      players: players,
+      to: volume_default,
+      from: volume_low,
+      increase: volume_space,
+    );
+  }
+
+  @override
+  Future<void> setVolumeDowns({
+    required List<GeneralLibraryPlayerControllerBase> players,
+    double from = 100,
+    required double to,
+    double decrease = 5,
+  }) async {
+    for (var i = 0; i < players.length; i++) {
+      await setVolumeDown(
+        player: players[i],
+        to: to,
+        from: from,
+        decrease: decrease,
+      );
+    }
+  }
+
+  @override
+  Future<void> setVolumeUps({
+    required List<GeneralLibraryPlayerControllerBase> players,
+    required double from,
+    double to = 100,
+    double increase = 5,
+  }) async {
+    for (var i = 0; i < players.length; i++) {
+      await setVolumeUp(
+        player: players[i],
+        to: to,
+        from: from,
+        increase: increase,
+      );
+    }
+  
   }
 }
