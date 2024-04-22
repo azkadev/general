de00eee20edea36d954f1366d9e7d96759c4b0b9
Your Name <you@example.com>
2024-04-22 17:49:18 +0700

    update

--- bin/generate/generate_docs.dart
+++ bin/generate/generate_docs.dart
@@ -1,12 +1,14 @@
+import 'dart:convert';
 import 'dart:io';
-
+ 
 import 'package:general_lib/script_generate/script_generate.dart';
 import 'package:path/path.dart';
 
 void main(List<String> args) async {
   List<ScriptGenerator> scipts = Directory(join(Directory.current.path, "docs")).listSync().toScriptGenerate();
-  String scipt = scipts.toScriptDart(scriptName: "general_docs");
-  File file = File(join(Directory.current.path, "generate", "docs", "general_docs.dart"));
+  String scipt = json.encode(scipts.toJson());
+  // scipts.toScriptDart(scriptName: "general_docs");
+  File file = File(join(Directory.current.path, "generate", "docs", "general_docs_mini.json"));
 
   await file.parent.create(
     recursive: true,
